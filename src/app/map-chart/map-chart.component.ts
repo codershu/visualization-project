dd8a1a723c4f09988b6ee0d98ba1c004ec38a9b5
Shu Han <shuhan@Shus-MacBook-Pro.local>
2021-02-24 11:24:04 +0800

    simple logic

--- src/app/map-chart/map-chart.component.ts
+++ src/app/map-chart/map-chart.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapChartComponent implements OnInit {
 
+  myNumber: number = 0;
+  myDate: string[] = ['2021-01-01', '2021-01-02', '2021-01-03'];
+  index: number = 0;
+  json: [] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.dynamicalChange();
   }
 
+  dynamicalChange(){
+    setInterval(() => {
+      let currentDate = this.myDate[this.index];
+      this.changeData(currentDate);
+      this.index += 1;
+    }, 1000);
+  }
+
+  changeData(date: string){
+    // d3.selectAll(this.json.find(x => x.date == date))
+    // .enter()
+    
+  }
 }
