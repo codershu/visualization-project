95a4dec3ede375f55f22f7d8eb9e2b53de36a7a3
Yanyan Cui <yoen@yanyantekiair.attlocal.net>
2021-02-26 17:59:38 -0600

    barchart

--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -1,20 +1,101 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, Input, OnChanges, OnInit, ViewChild } from '@angular/core';
+import { RouterState } from '@angular/router';
+import { IfControlStateService } from '@clr/angular/forms/common/if-control-state/if-control-state.service';
+import { constants } from 'buffer';
 import * as d3 from "d3";
+import { groups } from 'd3';
+import states from '../../assets/data/state_name.json';
+import { DailyData, MapSingleDayData, StateData } from '../shared/models';
 
 @Component({
   selector: 'app-bar-chart',
   templateUrl: './bar-chart.component.html',
   styleUrls: ['./bar-chart.component.css']
 })
-export class BarChartComponent implements OnInit {
+export class BarChartComponent implements OnInit, OnChanges {
+  @ViewChild('barChart')
+  private chartContainer: ElementRef;
+
+  @Input()
+  data: Data[];
+
+  margin = { top:20, right:20, bottom:30, left:40};
+
+
   
-  currentDate: string = "";
+  constructor( ) { }
 
-  constructor() { 
+  ngOnInit() {}
 
+
+  ngOnChanges(): void {
+    if (!this.data) { return; }
+    this.createChart();
+    
   }
 
-  ngOnInit(): void {
+  data = 
+
+  private createChart(): void{
+    d3.select('svg').remove();
+
+    const element = this.chartContainer.nativeElement;
+    const data = this.data;
+
+    const svg = d3.select(element)
+                  .append('svg')
+                  .attr('width', element.offsetWidth)
+                  .attr('height', element.offsetHeight);
+
+    const contentWidth = element.offsetWidth - this.margin.left - this.margin.right;
+    const contentHeight = element.offsetHeight - this.margin.top - this.margin.bottom;
+
+    const x = d3
+      .scaleBand()
+      .rangeRound([0, contentWidth])
+      .padding(0.1)
+      .domain(data.map(d => d.letter));
+
+    const y = d3
+      .scaleLinear()
+      .rangeRound([contentHeight, 0])
+      .domain([0, d3.max(data, d => d.frequency)]);
+
+    const g = svg.append('g')
+      .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
+
+    g.append('g')
+      .attr('class', 'axis axis--x')
+      .attr('transform', 'translate(0,' + contentHeight + ')')
+      .call(d3.axisBottom(x));
+
+    g.append('g')
+      .attr('class', 'axis axis--y')
+      .call(d3.axisLeft(y).ticks(10, '%'))
+      .append('text')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', 6)
+      .attr('dy', '0.71em')
+      .attr('text-anchor', 'end')
+      .text('Frequency');
+
+    g.selectAll('.bar')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', d => x(d.letter))
+      .attr('y', d => y(d.frequency))
+      .attr('width', x.bandwidth())
+      .attr('height', d => contentHeight - y(d.frequency));
   }
 
+
+  
+
+
+ 
+
+  
 }
